a4781bfcf9dfc1d689e1f23247cc340d69237cc7
Chellaa <98450157+chellaa18@users.noreply.github.com>
2023-12-01 10:28:46 +0530

    Add files via upload

---
+++ src/layout/DefaultLayout.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
+import Dashboard from 'src/views/dashboard/Dashboard'
+
+
+const DefaultLayout = () => {
+  return (
+    <div>
+      <AppSidebar />
+      <div className="wrapper d-flex flex-column min-vh-100 bg-light">
+        <AppHeader />
+        <div className="body flex-grow-1 px-3">
+          <Dashboard/>
+        </div>
+        <AppFooter />
+      </div>
+    </div>
+  )
+}
+
+export default DefaultLayout
---
+++ src/views/base/cards/Cards.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardGroup,
+  CCardHeader,
+  CCardImage,
+  CCardLink,
+  CCardSubtitle,
+  CCardText,
+  CCardTitle,
+  CListGroup,
+  CListGroupItem,
+  CNav,
+  CNavItem,
+  CNavLink,
+  CCol,
+  CRow,
+} from '@coreui/react'
+import { DocsExample } from 'src/components'
+
+import ReactImg from 'src/assets/images/react.jpg'
+
+const Cards = () => {
+  return (
+   <>
+   <h4>Card Section</h4></>
+  )
+}
+
+export default Cards
